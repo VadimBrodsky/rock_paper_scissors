f46d2c7aedb3d1684bd0012a7d5a0dae9c6f49e1
Vadim Brodsky <vadimb@crankworks.ca>
2015-05-29 09:15:47 -0400

    Add tests for all game rules

--- test/rock_paper_scissors_test.rb
+++ test/rock_paper_scissors_test.rb
@@ -7,6 +7,8 @@ class GameTest < Minitest::Test
     @h1 = Hand.new('r')
     @h2 = Hand.new('p')
     @h3 = Hand.new('sc')
+    @h4 = Hand.new('l')
+    @h5 = Hand.new('sp')
     @p1 = Player.new('Kirk',   @h1)
     @p2 = Player.new('Picard', @h2)
     @rules = Rules.new
@@ -35,5 +37,65 @@ class GameTest < Minitest::Test
   def test_tie_game
     data = Player.new('Data', @h1)
     assert_equal @rules.check(@p1, data).winner, nil
+  end
+
+  def test_rock_wins_lizard
+    p1 = Player.new('a', @h1)
+    p2 = Player.new('b', @h4)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def test_rock_wins_scissors
+    p1 = Player.new('a', @h1)
+    p2 = Player.new('b', @h3)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def test_paper_wins_rock
+    p1 = Player.new('a', @h2)
+    p2 = Player.new('b', @h1)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def test_paper_wins_spock
+    p1 = Player.new('a', @h2)
+    p2 = Player.new('b', @h5)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def scissors_wins_paper
+    p1 = Player.new('a', @h3)
+    p2 = Player.new('b', @h2)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def scissors_wins_lizard
+    p1 = Player.new('a', @h3)
+    p2 = Player.new('b', @h4)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def test_lizard_wins_paper
+    p1 = Player.new('a', @h4)
+    p2 = Player.new('b', @h2)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def test_lizard_wins_spock
+    p1 = Player.new('a', @h4)
+    p2 = Player.new('b', @h5)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def test_spock_wins_rock
+    p1 = Player.new('a', @h5)
+    p2 = Player.new('b', @h1)
+    assert_equal @rules.check(p1, p2).winner, 'a'
+  end
+
+  def test_spock_wins_scissors
+    p1 = Player.new('a', @h5)
+    p2 = Player.new('b', @h3)
+    assert_equal @rules.check(p1, p2).winner, 'a'
   end
 end
