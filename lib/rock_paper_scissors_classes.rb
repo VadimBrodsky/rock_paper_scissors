474e13cca87f32f7798d51427dc88a4cf5950f85
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-05-28 21:31:16 -0400

    Refactor method to OOP classes, add tests

--- lib/rock_paper_scissors_classes.rb
+++ lib/rock_paper_scissors_classes.rb
@@ -4,57 +4,69 @@ class Hand
   HANDS = { 'p' => 'Paper', 'r' => 'Rock', 'sc' => 'Scissors',
             'sp' => 'Spock', 'l' => 'Lizard' }
 
-  attr_accessor :shape, :description
+  attr_reader :shape, :description
 
-  def initialize
+  def initialize(chosen_shape = nil)
+    self.shape = chosen_shape
   end
 
   def throw_random
-    self.shape = HANDS.keys.sample
-    self.description = HANDS[self.shape]
+    @shape = HANDS.keys.sample
+    @description = HANDS[self.shape]
+    self
+  end
+
+  def shape=(chosen_shape)
+    if HANDS.key?(chosen_shape)
+      @shape = chosen_shape
+      @description = HANDS[chosen_shape]
+    end
+    self
   end
 end
 
 class Player
   attr_accessor :name, :hand
 
-  def initialize=(name)
+  def initialize(name = nil, hand = nil)
     self.name = name
+    self.hand = hand
   end
 end
 
 class Rules
-  WIN_CONDITIONS = { 'r' => ['l', 's'] ,'p' => ['r', 'sp'], 'sc' => ['p', 'l'],
-                     'l' => ['p', 'sp'], 'sp' => ['r', 'sc'] }
-
-  WIN_STATEMNTS = {['p',  'r' ]  => 'Paper wraps Rock!',
-                   ['p',  'sp']  => 'Paper disproves Spock!',
-                   ['r',  'sc']  => 'Rock smashes Scissors!',
-                   ['r',  'l' ]  => 'Rock crushes Lizard!',
-                   ['sc', 'p' ]  => 'Scissors cut Paper!',
-                   ['sc', 'l' ]  => 'Scissors cut Lizard!',
-                   ['l',  'p' ]  => 'Lizard eats Paper!',
-                   ['l',  'sp']  => 'Lizard poisons Spock!',
-                   ['sp', 'sc']  => 'Spock smashes Scissors!',
-                   ['sp', 'r' ]  => 'Spock vaporizes Rock!'}
-
-  attr_reader :winner, :statment
+  attr_reader :winner, :statement
 
   def initialize
   end
 
-  def check_rules(player_1, player_2)
+  def check(player_1, player_2)
     check_for_tie(player_1, player_2) if @winner.nil?
     check_winner(player_1, player_2)  if @winner.nil?
     check_winner(player_2, player_1)  if @winner.nil?
+    self
   end
 
   private
 
+    WIN_CONDITIONS = { 'r' => ['l', 's'] ,'p' => ['r', 'sp'], 'sc' => ['p', 'l'],
+                       'l' => ['p', 'sp'], 'sp' => ['r', 'sc'] }
+
+    WIN_STATEMENTS = {['p',  'r' ]  => 'Paper wraps Rock!',
+                      ['p',  'sp']  => 'Paper disproves Spock!',
+                      ['r',  'sc']  => 'Rock smashes Scissors!',
+                      ['r',  'l' ]  => 'Rock crushes Lizard!',
+                      ['sc', 'p' ]  => 'Scissors cut Paper!',
+                      ['sc', 'l' ]  => 'Scissors cut Lizard!',
+                      ['l',  'p' ]  => 'Lizard eats Paper!',
+                      ['l',  'sp']  => 'Lizard poisons Spock!',
+                      ['sp', 'sc']  => 'Spock smashes Scissors!',
+                      ['sp', 'r' ]  => 'Spock vaporizes Rock!'}
+
     def check_for_tie(player_1, player_2)
       if player_1.hand.shape == player_2.hand.shape
-        @winner  = 'None'
-        @statment = 'It\'s a tie.'
+        @winner  = nil
+        @statement = 'It\'s a tie.'
       end
     end
 
@@ -62,7 +74,7 @@ class Rules
       WIN_CONDITIONS.each do |key, value|
         if key == player_1.hand.shape && value.include?(player_2.hand.shape)
           @winner = player_1.name
-          @statement = WIN_STATEMNTS[player_1.hand.shape, player_2.hand.shape]
+          @statement = WIN_STATEMENTS[[player_1.hand.shape, player_2.hand.shape]]
           break
         end
       end
@@ -89,22 +101,23 @@ class Game
 
 end
 
-h1 = Hand.new
-h1.throw_random
-
-h2 = Hand.new
-h2.throw_random
-
-p1 = Player.new
-p2 = Player.new
-
-p1.name = 'Jonny'
-p1.hand = h1
-
-p2.name = 'Computer'
-p2.hand = h2
-
+# h1 = Hand.new
+# h1.throw_random
+#
+# h2 = Hand.new
+# h2.throw_random
+#
+# p1 = Player.new
+# p2 = Player.new
+#
+# p1.name = 'Jonny'
+# p1.hand = h1
+#
+# p2.name = 'Computer'
+# p2.hand = h2
+#
 # outcome = Rules.new
+#
+# binding.pry
+#
 # p outcome.check_rules(p1, p2)
-
-binding.pry
--- test/rock_paper_scissors_test.rb
+++ test/rock_paper_scissors_test.rb
@@ -1,63 +1,41 @@
 require 'minitest/autorun'
-require 'rock_paper_scissors_methods'
+require 'rock_paper_scissors_classes'
 
 # Testing BlackJack Methods
 class GameTest < Minitest::Test
   def setup
-    @deck_1 = create_deck(DECK)
-    @deck_2 = create_deck(DECK)
-    @bust_hand  = [%w(9 diamonds), %w(8 hearts), %w(10 diamonds)]
-    @small_hand = [%w(5 diamonds), %w(3 hearts)]
-    @large_hand = [%w(9 diamonds), %w(10 diamonds)]
-    @small_ace_hand = [%w(10 diamonds), %w(5 hearts), %w(ace spades)]
-    @large_ace_hand = [%w(3 diamonds), %w(5 hearts), %w(ace spades)]
-    @triple_aces_hand = [%w(ace spades), %w(ace hearts), %w(ace clubs)]
+    @h1 = Hand.new('r')
+    @h2 = Hand.new('p')
+    @h3 = Hand.new('sc')
+    @p1 = Player.new('Kirk',   @h1)
+    @p2 = Player.new('Picard', @h2)
+    @rules = Rules.new
   end
 
-  def test_two_decks_should_be_different
-    assert_equal @deck_1, @deck_1
-    refute_equal @deck_1, @deck_2
+  def test_player_init
+    assert_equal @p1.name, 'Kirk'
+    refute_equal @p1.name, 'SOMETHING ELSE'
   end
 
-  def test_removing_cards_should_remove_from_deck
-    full_deck = create_deck(DECK).count
-    partial_deck = give_cards_from(create_deck(DECK), 10).count
-    refute_equal full_deck, partial_deck
+  def test_hand_init_valid
+    valid_hand = Hand.new('r')
+    assert_equal valid_hand.shape, 'r'
   end
 
-  def test_values_of_ace
-    ace = CARDS['ace']
-    assert_equal value_of(ace, 3),  11
-    assert_equal value_of(ace, 10), 11
-    assert_equal value_of(ace, 11), 1
-    assert_equal value_of(ace, 15), 1
+  def test_hand_init_invalid
+    valid_hand = Hand.new('LAZER')
+    refute_equal valid_hand.shape, 'LAZER'
   end
 
-  def test_hitting_should_remove_card_from_deck
-    full_deck = create_deck(DECK)
-    full_deck_count = full_deck.count
-    hit([], full_deck)
-    partial_deck_count = full_deck.count
-    refute_equal full_deck_count, partial_deck_count
+  def test_player_should_win
+    assert_equal @rules.check(@p1, @p2).winner, @p2.name
+    assert_equal @rules.check(@p2, @p1).winner, @p2.name
   end
 
-  def test_hand_should_bust
-    assert bust?(@bust_hand), true
-    refute bust?(@small_hand), true
+  def test_tie_game
+    data = Player.new('Data', @h1)
+    assert_equal @rules.check(@p1, data).winner, nil
   end
 
-  def test_sum_of_cards
-    assert_equal sum_of(@small_hand), 8
-    assert_equal sum_of(@large_hand), 19
-    assert_equal sum_of(@large_ace_hand), 19
-    assert_equal sum_of(@small_ace_hand), 16
-    assert_equal sum_of(@triple_aces_hand), 13
-  end
 
-  def test_correct_winner
-    assert name_of_winner(@large_hand, @small_hand), 'Player'
-    assert name_of_winner(@small_hand, @bust_hand),  'Player'
-    assert name_of_winner(@small_hand, @large_hand), 'Dealer'
-    assert name_of_winner(@bust_hand, @small_hand), 'Dealer'
-  end
 end
